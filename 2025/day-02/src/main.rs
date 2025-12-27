5f29215f780cbebe0aaea327eb40db7884509f70
Cole Hughes <coltenhughes@gmail.com>
2025-12-27 10:06:48 -0500

    Split into part one/two functions.

--- 2025/day-02/src/main.rs
+++ 2025/day-02/src/main.rs
@@ -1,4 +1,17 @@
 
+fn part_one(counter: &mut i64, value: i64) {
+    let string = value.to_string();
+    if string.len() % 2 == 0 {
+        let (first, last) = string.split_at(string.len() / 2);
+        if first == last {
+            *counter += value;
+        }
+    }
+}
+
+fn part_two(counter: &i32, value: i32) {
+}
+
 fn main() {
     let contents: String = std::fs::read_to_string("input")
         .expect("Could not read input file.");
@@ -12,17 +25,11 @@ fn main() {
         })
         .collect();
 
-    let mut counter = 0;
+    let mut counter: i64 = 0;
 
     for bound in bounds.iter() {
         for value in bound.0..=bound.1 {
-            let string = value.to_string();
-            if string.len() % 2 == 0 {
-                let (first, last) = string.split_at(string.len() / 2);
-                if first == last {
-                    counter += value;
-                }
-            }
+            part_one(&mut counter, value);
         }
     }
 
