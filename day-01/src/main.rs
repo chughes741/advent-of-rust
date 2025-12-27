91b7c508471ab168530290d36becd1ff781caf47
Cole Hughes <coltenhughes@gmail.com>
2025-12-26 20:05:41 -0500

    Fixed intial index, correct answer.

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,10 +1,10 @@
 
 
 fn parse(instruction: &str) -> i32 {
-    let (direction, magnitude) = instruction.split_at(1);
+    let (direction, distance) = instruction.split_at(1);
 
     let sign = if direction == "R" { 1 } else { -1 };
-    let number = magnitude.parse::<i32>().expect("Could not parse to integer.");
+    let number = distance.parse::<i32>().expect("Could not parse to integer.");
 
     return sign * number;
 }
@@ -15,7 +15,7 @@ fn main() {
 
     let instructions: Vec<&str> = contents.split('\n').collect();
 
-    let mut index = 0;
+    let mut index = 50;
     let mut zero_count = 0;
 
     for instruction in instructions.iter() {
